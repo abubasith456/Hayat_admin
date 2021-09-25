c38b28510bb270174e783e5658e417f2330b4d52
arunvijay <arunvijay2885@gmail.com>
2021-09-25 11:55:29 +0530

    Tab bar make it scrollable and code cleaning

--- app/src/main/java/com/trizions/ui/dashboard/tab_fragments/projects/ProjectsTrackActivity.java
+++ app/src/main/java/com/trizions/ui/dashboard/tab_fragments/projects/ProjectsTrackActivity.java
@@ -1,27 +1,21 @@
 package com.trizions.ui.dashboard.tab_fragments.projects;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-
 import com.google.android.material.tabs.TabLayout;
 import com.trizions.BaseActivity;
-import com.trizions.BaseFragment;
 import com.trizions.R;
 import com.trizions.utils.PhotoPreViewActivity;
 
@@ -49,17 +43,38 @@ public class ProjectsTrackActivity extends BaseActivity {
         setContentView(R.layout.activity_projects_details);
         try {
             setupViewPager(viewpagerDetails);
-            tabLayoutDetails.setupWithViewPager(viewpagerDetails);
+            viewpagerDetails.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayoutDetails));
+            tabLayoutDetails.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+                @Override
+                public void onTabSelected(TabLayout.Tab tab) {
+                    viewpagerDetails.setCurrentItem(tab.getPosition());
+                }
+
+                @Override
+                public void onTabUnselected(TabLayout.Tab tab) {
+
+                }
+
+                @Override
+                public void onTabReselected(TabLayout.Tab tab) {
+
+                }
+            });
         } catch (Exception exception){
             Log.e("Error ==> ", "" + exception);
         }
     }
 
     private void setupViewPager(ViewPager viewPager) {
-        ProjectsTrackActivity.ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new ProjectsDetailsFragment(), "Current");
-        //adapter.addFragment(new ProjectsDetailsFragment(), "Completed");
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ProjectsDetailsFragment(), "Details");
+        adapter.addFragment(new ProjectsDetailsFragment(),"Report");
+        adapter.addFragment(new ProjectsDetailsFragment(),"Client");
+        adapter.addFragment(new ProjectsDetailsFragment(),"BOQ");
+        adapter.addFragment(new ProjectsDetailsFragment(),"Documents");
         viewPager.setAdapter(adapter);
+        tabLayoutDetails.setupWithViewPager(viewPager);
+
     }
 
     static class ViewPagerAdapter extends FragmentPagerAdapter {
